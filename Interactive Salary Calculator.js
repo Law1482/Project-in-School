4c399c06f01cf54cc730874655e4bbbb63a0306b
Law1482 <148838660+Law1482@users.noreply.github.com>
2023-11-23 12:22:22 +0800

    Add files via upload

---
+++ Interactive Salary Calculator.js
@@ -0,0 +1,34 @@
+var hourlyrate = document.getElementById("Hourlyrate");
+var hoursworked = document.getElementById("Hoursworked");
+var print = document.getElementById("printhere");
+var username = document.getElementById("username");
+var empty1 = document.getElementById("empty");
+var invalid1 = document.getElementById("valid1");
+var invalid2 = document.getElementById("valid2");
+
+
+function Calculate() {
+	var e = empty1.value;
+	var hr = hourlyrate.value;
+	var hw = hoursworked.value;
+	var un = username.value;
+	invalid1.innerHTML = "";
+	invalid2.innerHTML = "";
+	if(!(un === e || hr === e || hw === e)) {
+		if(hr > 0) {
+			if(hw > 0) {
+			var totalsalary = hr * hw;
+			print.innerHTML = "<b>Name : </b>" + un + "<br>" + "<b>Hourlyrate : </b>" + "Php" + hr + "<br>" + "<b>Hoursworked : </b>"  + hw + " Hour's" + "<br>" + "<b>Total Salary per day : </b>" + "Php" + totalsalary;	
+		    } else {
+		    	invalid2.innerHTML = "Invalid hoursworked";
+		    	hoursworked.value = "";
+		    }
+	    } else {
+	    	invalid1.innerHTML = "Invalid hourlyrate";
+	    	hourlyrate.value = "";
+	    	
+	    }
+	} else {
+		alert("please fill before Calculate it.");
+	}	
+}
