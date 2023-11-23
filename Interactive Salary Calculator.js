a3f471d2897664caa4915d0e997e16710bfd0e73
Law1482 <148838660+Law1482@users.noreply.github.com>
2023-11-23 08:09:06 +0800

    Add files via upload

---
+++ Interactive Salary Calculator.js
@@ -0,0 +1,7 @@
+var hourlyrate = document.getElementById("Hourlyrate");
+var hoursworked = document.getElementById("Hoursworked");
+
+function calculate() {
+				var totalsalary = hoursworked
+				
+}
